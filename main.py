88a13c2310b3f56d035947dd072a22bb7fb0a55b
mishakuschenko <liahimkusenko@gmail.com>
2024-11-21 23:22:33 +0300

    Добавил обработчик инлайн кнопок, исправил первые баги. Я спать

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from os import getenv
 from dotenv import load_dotenv
 import handlers 
 
+
 load_dotenv()
-global bot = telebot.TeleBot(getenv('TOKEN'))
+bot = telebot.TeleBot(getenv('TOKEN'))
 
 def parse_handlers():
     handlers.start_handler(bot)
+    handlers.callback_inline(bot)
 
 if __name__ == "__main__":
+    parse_handlers()
     bot.infinity_polling()
