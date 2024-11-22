051abb42f3f5b257a5ba6d9f3f53ca66fb6fcc48
mishakuschenko <liahimkusenko@gmail.com>
2024-11-22 19:54:05 +0300

    синхронизировал меня с денисом

--- handlers.py
+++ handlers.py
@@ -1,26 +1,4 @@
 from lib.files import ReadFile
-from telebot import types
-from main import bot
 
-read = ReadFile()
-
-def start_handler(bot):
-    @bot.message_handler(commands=["start"])
-    def start(msg):
-        inline_kbd = types.InlineKeyboardMarkup(row_width=3)
-        start_button = types.InlineKeyboardButton(text="тестовая кнопка", callback_data="START")
-        inline_kbd.add(start_button)
-        bot.send_message(msg.chat.id, read.read("./texts/start.txt"), reply_markup=inline_kbd)
-
-
-def callback_inline(bot):
-    @bot.callback_query_handler(func=lambda call: True)
-    def callback_inline(call):
-        call_id = call.message.chat.id
-
-        if call.message:
-            if call.data == "START":
-                bot.send_message(call_id, "all is good!")
-                bot.answer_callback_query(callback_query_id=call.id, text="")
-
+r = ReadFile()
 
--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-import telebot #проверяю
 from os import getenv
 from dotenv import load_dotenv
-import handlers 
 
 
 load_dotenv()
 bot = telebot.TeleBot(getenv('TOKEN'))
 
-def parse_handlers():
-    handlers.start_handler(bot)
-    handlers.callback_inline(bot)
 
-if __name__ == "__main__":
-    parse_handlers()
-    bot.infinity_polling()
