96922f80784013e7ac1809f13da002b54b04a36d
mishakuschenko <liahimkusenko@gmail.com>
2024-11-22 20:00:26 +0300

    проверил либу, все норм

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from os import getenv
 from dotenv import load_dotenv
-
+from aiogram import Bot, Dispatcher, types
+import asyncio
 
 load_dotenv()
-bot = telebot.TeleBot(getenv('TOKEN'))
+bot = Bot(getenv('TOKEN'))
+dsp = Dispatcher()
 
+@dsp.message()
+async def start(message: types.Message):
+    await message.answer("ты чета написал")
+
+
+if __name__ == "__main__":
+    asyncio.run(dsp.start_polling(bot))
 
